c08a8e54315254e8c4feb790be73576d6d8ec617
daniela velez <danielavelez232mail.com>
2024-08-27 20:44:05 -0500

    entrenamientospringboot

---
+++ src/main/java/com/entrenamientospring/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package com.entrenamientospring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.entrenamientospring.interfaceServices.InterfaceUsuarioServices;
+import com.entrenamientospring.models.Usuario;
+
+@Controller
+@RequestMapping
+public class UsuarioController {
+	
+	@Autowired
+	private InterfaceUsuarioServices service;
+	
+	@GetMapping("Usuario/listar")
+	public String listar(Model model) {
+		List<Usuario>usuario=service.listar();
+		model.addAttribute("usuario", usuario);
+		return "Usuario/listar";
+	}
+	
+	
+	@GetMapping("Usuario/insertar")
+	public String VistaGuardar(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "Usuario/insertar";
+	}
+
+	@PostMapping("Usuario/guardar")
+	public String guardar(@Validated Usuario u,Model model) {
+		service.guardar(u);
+		return "redirect:/Usuario/listar";
+	}
+	
+	
+	@GetMapping("Usuario/Borrar/{id}")
+	public String borrar(@PathVariable int id, Model model) {
+	service.borrar(id);	
+	return "redirect:/Usuario/listar";
+	}
+}
---
+++ src/main/java/com/entrenamientospring/interfaces/UsuarioInterface.java
@@ -0,0 +1,12 @@
+package com.entrenamientospring.interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+import com.entrenamientospring.models.Usuario;
+
+
+public interface UsuarioInterface extends CrudRepository<Usuario, Integer> {
+
+	
+}
